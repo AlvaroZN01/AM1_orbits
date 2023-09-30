2d20545daa205b5691cbb5d984c7f24aa06646db
AlvaroZN01 <alvarozn01@gmail.com>
2023-09-30 20:45:57 +0200

    Hito 2: Apartado 5

--- Hito_2/Hito_2.py
+++ Hito_2/Hito_2.py
@@ -28,23 +28,34 @@ def F_EI(x, F, dt, t0, U0):
 
 
 
+def Cauchy(Solver, F, U0, t0, dt, N):
+    U = array(zeros((len(U0),N)))
+    U[:,0] = U0
+    t = t0
+    for ii in range(0, N - 1):
+        U[:,ii+1] = Solver(F, dt, t, U[:,ii])
+        t = t + dt
+    return U
+
+
+
+# Function to be integrated
 def F_Kepler(t, U):
     
     x, y, vx, vy = U[0], U[1], U[2], U[3]
     mr = (x**2 + y**2)**1.5
     return array([vx, vy, -x/mr, -y/mr])
 
-
-
+# Integration parameters definition
 N = 10000
 dt = 0.001
-U = array(zeros((4,N)))
-U[:,0] = [1, 0, 0, 1]
+U0 = [1, 0, 0, 1]
+t0 = 0
 
+# Cauchy problem solver
+U = Cauchy(Inverse_Euler, F_Kepler, U0, t0, dt, N)
 
-for ii in range(0, N-1):
-    U[:,ii+1] = Inverse_Euler(F_Kepler, dt, 0, U[:,ii])
-
+# Plot results
 plt.axis('equal')
 plt.plot(U[0,:],U[1,:])
 plt.show()
