c160ffd460f2fcd727bd96c7bdb1fccc827c7bbd
AlvaroZN01 <alvarozn01@gmail.com>
2023-10-05 13:02:53 +0200

    Se ha deshecho el cambio de dt  y N por t y t_total. Se ha cambiado las F por f en los esquemas de integración.

--- Hito_2/Hito_2.py
+++ Hito_2/Hito_2.py
@@ -4,35 +4,35 @@ from scipy import optimize
 
 
 # Temporal schemes to integrate a Cauchy problem
-def Euler(F, dt, t0, U0):
-    return U0 + dt * F(t0, U0)
+def Euler(f, dt, t0, U0):
+    return U0 + dt * f(t0, U0)
 
-def Crank_Nicolson(F, dt, t0, U0):
-    return optimize.newton(func = F_CK, x0 = U0, fprime = None, args = (F, dt, t0, U0))
+def Crank_Nicolson(f, dt, t0, U0):
+    return optimize.newton(func = F_CK, x0 = U0, fprime = None, args = (f, dt, t0, U0))
 
-def F_CK(x, F, dt, t0, U0):
-    return x - U0 - dt/2 * (F(t0, U0) + F(t0 + dt, x))
+def F_CK(x, f, dt, t0, U0):
+    return x - U0 - dt/2 * (f(t0, U0) + f(t0 + dt, x))
 
-def RK4(F, dt, t0, U0):
-    k1 = F(t0, U0)
-    k2 = F(t0 + dt/2, U0 + k1*dt/2)
-    k3 = F(t0 + dt/2, U0 + k2*dt/2)
-    k4 = F(t0 + dt, U0 + k3*dt)
+def RK4(f, dt, t0, U0):
+    k1 = f(t0, U0)
+    k2 = f(t0 + dt/2, U0 + k1*dt/2)
+    k3 = f(t0 + dt/2, U0 + k2*dt/2)
+    k4 = f(t0 + dt, U0 + k3*dt)
     return U0 + dt/6 * (k1 + 2*k2 + 2*k3 + k4)
 
-def Inverse_Euler(F, dt, t0, U0):
-    return optimize.newton(func = F_EI, x0 = U0, fprime = None, args = (F, dt, t0, U0))
+def Inverse_Euler(f, dt, t0, U0):
+    return optimize.newton(func = F_EI, x0 = U0, fprime = None, args = (f, dt, t0, U0))
 
-def F_EI(x, F, dt, t0, U0):
-    return x - U0 - dt * F(t0 + dt, x)
+def F_EI(x, f, dt, t0, U0):
+    return x - U0 - dt * f(t0 + dt, x)
 
 # Cauchy problem solver
-def Cauchy(Solver, F, U0, t0, dt, N):
+def Cauchy(Solver, f, U0, t0, dt, N):
     U = array(zeros((len(U0),N)))
     U[:,0] = U0
     t = t0
     for ii in range(0, N - 1):
-        U[:,ii+1] = Solver(F, dt, t, U[:,ii])
+        U[:,ii+1] = Solver(f, dt, t, U[:,ii])
         t = t + dt
     return U
 
@@ -46,9 +46,8 @@ def F_Kepler(t, U):
     return array([vx, vy, -x/mr, -y/mr])
 
 # Integration parameters definition
-t_total = 10
 dt = 0.001
-N = t_total / dt
+N = 10000
 U0 = [1, 0, 0, 1]
 t0 = 0
 
