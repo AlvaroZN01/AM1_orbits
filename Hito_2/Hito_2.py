7709aa21d70bbe05a9ea8db616a6b3f553de68ac
AlvaroZN01 <alvarozn01@gmail.com>
2023-10-05 13:14:06 +0200

    Se ha modificado el orden de los inputs de los esquemas temporales. También se han cambiado los imputs dt y t0 por t0 y tf.

--- Hito_2/Hito_2.py
+++ Hito_2/Hito_2.py
@@ -4,27 +4,28 @@ from scipy import optimize
 
 
 # Temporal schemes to integrate a Cauchy problem
-def Euler(f, dt, t0, U0):
-    return U0 + dt * f(t0, U0)
+def Euler(U0, t0, tf, f):
+    return U0 + (tf - t0) * f(t0, U0)
 
-def Crank_Nicolson(f, dt, t0, U0):
-    return optimize.newton(func = F_CK, x0 = U0, fprime = None, args = (f, dt, t0, U0))
+def Crank_Nicolson(U0, t0, tf, f):
+    return optimize.newton(func = F_CK, x0 = U0, fprime = None, args = (U0, t0, tf, f))
 
-def F_CK(x, f, dt, t0, U0):
-    return x - U0 - dt/2 * (f(t0, U0) + f(t0 + dt, x))
+def F_CK(x, U0, t0, tf, f):
+    return x - U0 - (tf - 0)/2 * (f(t0, U0) + f(tf, x))
 
-def RK4(f, dt, t0, U0):
+def RK4(U0, t0, tf, f):
+    dt = tf - t0
     k1 = f(t0, U0)
     k2 = f(t0 + dt/2, U0 + k1*dt/2)
     k3 = f(t0 + dt/2, U0 + k2*dt/2)
     k4 = f(t0 + dt, U0 + k3*dt)
     return U0 + dt/6 * (k1 + 2*k2 + 2*k3 + k4)
 
-def Inverse_Euler(f, dt, t0, U0):
-    return optimize.newton(func = F_EI, x0 = U0, fprime = None, args = (f, dt, t0, U0))
+def Inverse_Euler(U0, t0, tf, f):
+    return optimize.newton(func = F_EI, x0 = U0, fprime = None, args = (U0, t0, tf, f))
 
-def F_EI(x, f, dt, t0, U0):
-    return x - U0 - dt * f(t0 + dt, x)
+def F_EI(x, U0, t0, tf, f):
+    return x - U0 - (t0 - tf) * f(tf, x)
 
 # Cauchy problem solver
 def Cauchy(Solver, f, U0, t0, dt, N):
@@ -32,7 +33,7 @@ def Cauchy(Solver, f, U0, t0, dt, N):
     U[:,0] = U0
     t = t0
     for ii in range(0, N - 1):
-        U[:,ii+1] = Solver(f, dt, t, U[:,ii])
+        U[:,ii+1] = Solver(U[:,ii], t, t + dt, f)
         t = t + dt
     return U
 
