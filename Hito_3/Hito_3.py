47d55e5b2be483ba8ac88adda4c48db752cc4e16
AlvaroZN01 <alvarozn01@gmail.com>
2023-10-14 13:21:51 +0200

    Hito 3 creado. Primer intento de conseguir q, pero la recta sale con pendiente positiva.

--- Hito_3/Hito_3.py
+++ Hito_3/Hito_3.py
@@ -1,4 +1,4 @@
-from numpy import arange, array, zeros, linalg
+from numpy import arange, array, zeros, linalg, log10
 import matplotlib.pyplot as plt
 from scipy import optimize
 
@@ -27,12 +27,29 @@ def Inverse_Euler(U0, t0, tf, f):
 
 # Cauchy problem solver
 def Cauchy(t, temporal_scheme, f, U0):
-    U = array (zeros((len(U0),len(t))))
+    U = array(zeros((len(U0),len(t))))
     U[:,0] = U0
     for ii in range(0, N - 1):
         U[:,ii+1] = temporal_scheme(U[:,ii], t[ii], t[ii+1], f)
     return U
 
+# Error evaluation using Richardson extrapolation 
+def error_eval(tn, temporal_scheme, f, U0):
+    t2n = array(zeros((len(t)*2)))
+    t2n[0] = tn[0]
+    for ii in range(1, len(t)):
+        t2n[ii*2] = tn[ii]
+        t2n[ii*2-1] = (tn[ii] + tn[ii-1]) / 2
+    U = Cauchy(tn, temporal_scheme, f, U0)
+    V = Cauchy(t2n, temporal_scheme, f, U0)
+    log_norm_E_n = array(zeros((len(t))))
+    for ii in range(0, len(t)):
+        log_norm_E_n[ii] = log10(linalg.norm(V[:,ii*2] - U[:,ii]))
+    n_vec = arange(0, len(t), 1)
+    n_vec = log10(n_vec)
+    plt.plot(n_vec, log_norm_E_n) # Según las gráficas de clase, debería salir con pendiente negativa.
+    plt.show()
+    return n_vec
 
 
 # Function to be integrated
@@ -50,10 +67,12 @@ t = arange(t0, N * dt, dt)
 U0 = [1, 0, 0, 1]
 
 # Cauchy problem solver
-U = Cauchy(t, Crank_Nicolson, F_Kepler, U0)
+#U = Cauchy(t, Crank_Nicolson, F_Kepler, U0)
+
+# Richardson extrapolation
+n = error_eval(t, Euler, F_Kepler, U0)
 
 # Plot results
-plt.axis('equal')
-plt.plot(U[0,:],U[1,:])
-plt.show()
-
+# plt.axis('equal')
+# plt.plot(U[0,:],U[1,:])
+# plt.show()
