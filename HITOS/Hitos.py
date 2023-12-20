a1f07a5cd8475e43c1569e4b535272b29cbf436d
AlvaroZN01 <alvarozn01@gmail.com>
2023-12-20 19:43:33 +0100

    Rango de hitos validos cambiado, el hito 7 se pondrá en una carpeta aparte

--- HITOS/Hitos.py
+++ HITOS/Hitos.py
@@ -12,8 +12,6 @@ elif hito_exe == 5:
     import Archivos_hitos.Hito_5
 elif hito_exe == 6:
     import Archivos_hitos.Hito_6
-# elif hito_exe == 7:
-    # import Hitos.Hito_7
 else:
-    print('Numero de hito incorrecto, entradas válidas del 1 al 7')
+    print('Numero de hito incorrecto, entradas válidas del 1 al 6')
     exit
